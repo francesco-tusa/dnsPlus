2dd07e4ecaefe2f758176cef3a9a52c533477e6f
francesco-tusa <francesco.tusa@gmail.com>
2025-01-06 10:39:49 +0000

    Added file missing from previous commit

--- src/experiments/cache/DNSWithCacheExperiment.java
+++ src/experiments/cache/DNSWithCacheExperiment.java
@@ -1,159 +1,46 @@
 package experiments.cache;
 
-import encryption.HEPS;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import broker.tree.binarybalanced.cache.BrokerWithBinaryBalancedTreeAndCache;
-import publishing.Publication;
-import publishing.Publisher;
-import subscribing.Subscriber;
+import experiments.RunTasksOutputManager;
+import experiments.SynchronousExperiment;
+import experiments.cache.DNSWithCacheRun.PublisherTask;
+import experiments.cache.DNSWithCacheRun.SubscriberTask;
+import java.util.List;
 
 /**
  *
- * @author f.tusa
+ * @author uceeftu
  */
-public final class DNSWithCacheExperiment 
-{
-    // may use a list for each type of entity
-    HEPS heps = HEPS.getInstance();
+public final class DNSWithCacheExperiment extends SynchronousExperiment {
+
+    private int numberOfPublications;
+    private int numberOfSubscriptions;
+
+    public DNSWithCacheExperiment(String inputFileName, int numberOfRuns) {
+        this(inputFileName, numberOfRuns, 100, 10);
+    }
+
+    public DNSWithCacheExperiment(String inputFileName, int numberOfRuns, int numberOfPublications, int numberOfSubscriptions) {
+        super(DNSWithCacheExperiment.class.getSimpleName(), inputFileName, numberOfRuns);
+        this.numberOfPublications = numberOfPublications;
+        this.numberOfSubscriptions = numberOfSubscriptions;
+    }
+
+    @Override
+    protected void executeRun() {
+        DNSWithCacheRun experimentRun = new DNSWithCacheRun(getDomainsDB(), numberOfPublications, numberOfSubscriptions);
+        experimentRun.setUp();
         
-    Subscriber subscriber = new Subscriber("Subscriber1");
-    Publisher publisher = new Publisher("Publisher1");
-    BrokerWithBinaryBalancedTreeAndCache broker = new BrokerWithBinaryBalancedTreeAndCache("Broker1", heps);
-    
-    File serviceNames;
-    String[] services;
-    
-    double cachePublications = 0;
-    Map<String, Double> matchTimings = new HashMap<>();
+        DNSWithCacheRun.SubscriberTask subscriberTask = experimentRun.new SubscriberTask("Sub1");
+        DNSWithCacheRun.PublisherTask publisherTask = experimentRun.new PublisherTask("Pub");
         
+        experimentRun.addTask(subscriberTask);
+        experimentRun.addTask(publisherTask);
+        
+        experimentRun.start();
        
-    
-    public DNSWithCacheExperiment(String file)
-    {
-        serviceNames = new File(file);
-        services = new String[1000];
+        experimentRun.cleanUp();
         
-        subscriber.setHeps(heps);
-        publisher.setHeps(heps);
-        
-        subscriber.getSecurityParameters();
-        publisher.getSecurityParameters();
-    }
-    
-    
-    public void simpleTest() {
-        System.out.println("Publication: fonts");
-        broker.matchPublication(publisher.generatePublication("fonts.googleapis.com"));
-     
-        System.out.println("Publication: facebook");
-        broker.matchPublication(publisher.generatePublication("facebook.com"));
-        
-        System.out.println("Publication: facebook");
-        broker.matchPublication(publisher.generatePublication("facebook.com"));
-        
-        System.out.println("Subscription facebook");
-        broker.cacheLookUp(subscriber.generateSubscription("facebook.com"));
-        
-        System.out.println("Subscription facebook");
-        broker.cacheLookUp(subscriber.generateSubscription("facebook.com")); 
-        
-        System.out.println("Publication: facebook");
-        broker.matchPublication(publisher.generatePublication("facebook.com"));
-    }
-    
-    public void generatePublications() throws FileNotFoundException 
-    {
-        long t;
-        int i = 0;
-        
-        try (Scanner scanner = new Scanner(serviceNames)) {
-            while (scanner.hasNextLine())
-            {
-                String service = scanner.nextLine();
-                services[i++] = service;
-                System.out.println("Publication: " + service);
-                Publication p = publisher.generatePublication(service);
-                t = System.nanoTime();
-                broker.matchPublication(p);
-                cachePublications += (System.nanoTime() - t);
-            }
-        }
-    }
-    
-//    public boolean generateSubscriptions(String service, Publication p) 
-//    {
-//        double timing = matchTimings.get(service);
-//        
-//        long t = System.nanoTime();
-//        boolean matchResult = broker.cacheLookUp(p);
-//        timing += (System.nanoTime() - t);
-//        
-//        matchTimings.put(service, timing);
-//        return matchResult;
-//    }
-//    
-//    
-//    public static void main(String[] args) 
-//    {    
-//        int iterations = 1;
-//        double subscriptions = 0;
-//             
-//        long t;
-//        
-//        String home = System.getProperty("user.home");
-//        DNSWithCacheExperiment dnsPlus = new DNSWithCacheExperiment(home + "/websites.txt");
-//
-//        try {
-//            System.out.println("*** Generating subscriptions table ***");
-//            t = System.nanoTime();
-//            dnsPlus.generateSubscriptions(); 
-//            subscriptions = System.nanoTime() - t;
-//            
-//        } catch (FileNotFoundException ex) {
-//            System.out.println("Subscription could not be loaded: " + ex.getMessage());
-//            System.exit(-1);
-//        }
-//        
-//        System.out.println();
-//        System.out.println("*** Matching publications ***");
-//
-//        for (String service : dnsPlus.matchTimings.keySet())
-//        {
-//            Publication publication = dnsPlus.publisher.generatePublication(service);
-//
-//            for (int i=0; i<iterations; i++) 
-//            {
-//                System.out.println(service + ": " + dnsPlus.match(service, publication));
-//            }
-//        }
-//        
-//        System.out.println();
-//        System.out.println("Total Subscriptions table generation (ms) [includes subscriptions generation]: " + subscriptions / 1000000);
-//        System.out.println("Subscriptions table generation (ms) [actual time to add subscriptions to the table]: " + dnsPlus.cachePublications / 1000000);
-//        System.out.println();
-//        double timingSum = 0;
-//        for (String service : dnsPlus.matchTimings.keySet()) 
-//        {
-//            double timing = (dnsPlus.matchTimings.get(service) / 1000000) / iterations;
-//            System.out.println("Match [" + service + "] (ms): " + String.format("%.2f", timing));
-//            timingSum += timing;
-//        }
-//        
-//        System.out.println();
-//        System.out.println("Average match time (ms): " + timingSum / dnsPlus.matchTimings.size());
-//    } 
-    
-    
-    public static void main(String[] args) 
-    {
-        String home = System.getProperty("user.home");
-        DNSWithCacheExperiment dnsPlus = new DNSWithCacheExperiment(home + "/websites.txt");
-        
-        dnsPlus.simpleTest();
-        
+        List<RunTasksOutputManager> allRunsTasksOutput = getAllRunsTasksOutput();
+        allRunsTasksOutput.add(experimentRun);
     }
 }
